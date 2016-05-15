e89f658bb1c1a690628d79f7825bc119acf5070c
James Sullivan <you@example.com>
2016-05-15 17:27:34 -0400

    first commit

---
+++ LoadAnimation/Form1.Designer.vb
@@ -0,0 +1,173 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.img1 = New System.Windows.Forms.PictureBox()
+        Me.img2 = New System.Windows.Forms.PictureBox()
+        Me.img3 = New System.Windows.Forms.PictureBox()
+        Me.img4 = New System.Windows.Forms.PictureBox()
+        Me.img5 = New System.Windows.Forms.PictureBox()
+        Me.img6 = New System.Windows.Forms.PictureBox()
+        Me.img7 = New System.Windows.Forms.PictureBox()
+        Me.img8 = New System.Windows.Forms.PictureBox()
+        CType(Me.img1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.img2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.img3, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.img4, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.img5, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.img6, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.img7, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.img8, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Timer1
+        '
+        Me.Timer1.Enabled = True
+        '
+        'img1
+        '
+        Me.img1.Image = CType(resources.GetObject("img1.Image"), System.Drawing.Image)
+        Me.img1.InitialImage = CType(resources.GetObject("img1.InitialImage"), System.Drawing.Image)
+        Me.img1.Location = New System.Drawing.Point(12, 12)
+        Me.img1.Name = "img1"
+        Me.img1.Size = New System.Drawing.Size(128, 128)
+        Me.img1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.img1.TabIndex = 0
+        Me.img1.TabStop = False
+        '
+        'img2
+        '
+        Me.img2.Image = CType(resources.GetObject("img2.Image"), System.Drawing.Image)
+        Me.img2.Location = New System.Drawing.Point(12, 12)
+        Me.img2.Name = "img2"
+        Me.img2.Size = New System.Drawing.Size(128, 128)
+        Me.img2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.img2.TabIndex = 1
+        Me.img2.TabStop = False
+        '
+        'img3
+        '
+        Me.img3.Image = CType(resources.GetObject("img3.Image"), System.Drawing.Image)
+        Me.img3.Location = New System.Drawing.Point(12, 12)
+        Me.img3.Name = "img3"
+        Me.img3.Size = New System.Drawing.Size(128, 128)
+        Me.img3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.img3.TabIndex = 2
+        Me.img3.TabStop = False
+        '
+        'img4
+        '
+        Me.img4.Image = CType(resources.GetObject("img4.Image"), System.Drawing.Image)
+        Me.img4.Location = New System.Drawing.Point(12, 12)
+        Me.img4.Name = "img4"
+        Me.img4.Size = New System.Drawing.Size(128, 128)
+        Me.img4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.img4.TabIndex = 3
+        Me.img4.TabStop = False
+        '
+        'img5
+        '
+        Me.img5.Image = CType(resources.GetObject("img5.Image"), System.Drawing.Image)
+        Me.img5.Location = New System.Drawing.Point(12, 12)
+        Me.img5.Name = "img5"
+        Me.img5.Size = New System.Drawing.Size(128, 128)
+        Me.img5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.img5.TabIndex = 4
+        Me.img5.TabStop = False
+        '
+        'img6
+        '
+        Me.img6.Image = CType(resources.GetObject("img6.Image"), System.Drawing.Image)
+        Me.img6.Location = New System.Drawing.Point(12, 12)
+        Me.img6.Name = "img6"
+        Me.img6.Size = New System.Drawing.Size(128, 128)
+        Me.img6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.img6.TabIndex = 5
+        Me.img6.TabStop = False
+        '
+        'img7
+        '
+        Me.img7.Image = CType(resources.GetObject("img7.Image"), System.Drawing.Image)
+        Me.img7.Location = New System.Drawing.Point(12, 12)
+        Me.img7.Name = "img7"
+        Me.img7.Size = New System.Drawing.Size(128, 128)
+        Me.img7.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.img7.TabIndex = 6
+        Me.img7.TabStop = False
+        '
+        'img8
+        '
+        Me.img8.Image = CType(resources.GetObject("img8.Image"), System.Drawing.Image)
+        Me.img8.Location = New System.Drawing.Point(12, 12)
+        Me.img8.Name = "img8"
+        Me.img8.Size = New System.Drawing.Size(128, 128)
+        Me.img8.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.img8.TabIndex = 7
+        Me.img8.TabStop = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.Window
+        Me.ClientSize = New System.Drawing.Size(155, 152)
+        Me.ControlBox = False
+        Me.Controls.Add(Me.img8)
+        Me.Controls.Add(Me.img7)
+        Me.Controls.Add(Me.img6)
+        Me.Controls.Add(Me.img5)
+        Me.Controls.Add(Me.img4)
+        Me.Controls.Add(Me.img3)
+        Me.Controls.Add(Me.img2)
+        Me.Controls.Add(Me.img1)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Loading..."
+        CType(Me.img1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.img2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.img3, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.img4, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.img5, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.img6, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.img7, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.img8, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents img1 As PictureBox
+    Friend WithEvents img2 As PictureBox
+    Friend WithEvents img3 As PictureBox
+    Friend WithEvents img4 As PictureBox
+    Friend WithEvents img5 As PictureBox
+    Friend WithEvents img6 As PictureBox
+    Friend WithEvents img7 As PictureBox
+    Friend WithEvents img8 As PictureBox
+End Class
---
+++ LoadAnimation/Form1.vb
@@ -0,0 +1,101 @@
+﻿Public Class Form1
+    Dim TimeInt As Integer
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        TimeInt = 0
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+
+        TimeInt = TimeInt + 1
+
+        If TimeInt = 1 Then
+            img1.Visible = True
+            img2.Visible = False
+            img3.Visible = False
+            img4.Visible = False
+            img5.Visible = False
+            img6.Visible = False
+            img7.Visible = False
+            img8.Visible = False
+        End If
+
+        If TimeInt = 2 Then
+            img1.Visible = False
+            img2.Visible = True
+            img3.Visible = False
+            img4.Visible = False
+            img5.Visible = False
+            img6.Visible = False
+            img7.Visible = False
+            img8.Visible = False
+        End If
+
+        If TimeInt = 3 Then
+            img1.Visible = False
+            img2.Visible = False
+            img3.Visible = True
+            img4.Visible = False
+            img5.Visible = False
+            img6.Visible = False
+            img7.Visible = False
+            img8.Visible = False
+        End If
+
+        If TimeInt = 4 Then
+            img1.Visible = False
+            img2.Visible = False
+            img3.Visible = False
+            img4.Visible = True
+            img5.Visible = False
+            img6.Visible = False
+            img7.Visible = False
+            img8.Visible = False
+        End If
+
+        If TimeInt = 5 Then
+            img1.Visible = False
+            img2.Visible = False
+            img3.Visible = False
+            img4.Visible = False
+            img5.Visible = True
+            img6.Visible = False
+            img7.Visible = False
+            img8.Visible = False
+        End If
+
+        If TimeInt = 6 Then
+            img1.Visible = False
+            img2.Visible = False
+            img3.Visible = False
+            img4.Visible = False
+            img5.Visible = False
+            img6.Visible = True
+            img7.Visible = False
+            img8.Visible = False
+        End If
+
+        If TimeInt = 7 Then
+            img1.Visible = False
+            img2.Visible = False
+            img3.Visible = False
+            img4.Visible = False
+            img5.Visible = False
+            img6.Visible = False
+            img7.Visible = True
+            img8.Visible = False
+        End If
+
+        If TimeInt = 8 Then
+            img1.Visible = False
+            img2.Visible = False
+            img3.Visible = False
+            img4.Visible = False
+            img5.Visible = False
+            img6.Visible = False
+            img7.Visible = False
+            img8.Visible = True
+            TimeInt = 0
+        End If
+    End Sub
+
+End Class
